28e22e7777d8b879d0d823f44c11df02f3c5914a
Vonn Joebert B. Achoy <vjachoy@gmail.com>
2024-03-12 14:56:35 +0800

    import footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import  Header  from '@/components/Header'
-
+import Footer from '@/components/pages/home/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <Header />
+      <Footer />
       {children}
       </body>
     </html>
