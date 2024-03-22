d2612c58a4c8f446fbcabc8023fc5ad8ce65e437
Vonn Joebert B. Achoy <vjachoy@gmail.com>
2024-03-22 10:31:46 +0800

    Organized folder and layout

--- app/(contact)/layout.tsx
+++ app/(contact)/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import Header from '@/components/Header'
+import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Header />
       {children}
       </body>
     </html>
---
+++ module/Transparency/Tranparency.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tranparency = () => {
+  return (
+    <div>Tranparency</div>
+  )
+}
+
+export default Tranparency
